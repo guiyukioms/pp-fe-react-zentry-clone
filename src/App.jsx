5fbd782938b7ac5da810729089daeb1cafb4b8dc
GuilhermeYukioMS <guiyukioms@gmail.com>
2024-12-12 15:46:26 -0300

    feat: add initial tailwind css config

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@
 const App = () => {
   return (
     <main>
-      <h1 className="text-5xl text-orange-500 font-bold">Bem Vind@!</h1>
+      <h1 className="text-5xl text-violet-300 font-bold">Bem Vind@!</h1>
     </main>
   )
 }
